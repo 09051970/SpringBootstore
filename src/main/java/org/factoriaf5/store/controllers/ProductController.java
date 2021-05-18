48b92b6b29f22f80496362590235cb9ec382725a
Pepi Martín <mjmartinmonzonis@hotmail.com>
2021-05-18 14:07:33 +0200

    quitando say hello

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -11,9 +11,6 @@ import java.util.List;
 
 @RestController
 public class ProductController {
-    @GetMapping("/health")
-    public String check() { return "Hello I´m a server";
-    }
 
     public List<Product> products = new ArrayList<>();
 
